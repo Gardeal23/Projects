a72ad00078c0f75e33dd9902651ec1e71b527a09
Uday Thapar <54276743+Gardeal23@users.noreply.github.com>
2020-06-02 04:02:27 -0400

    Added comments

--- create_folders.py
+++ create_folders.py
@@ -1,14 +1,17 @@
 import os
 
+#Utilizing the os library to make directory changes 
+#This specific program was intended to be used to make multiple folders at once, rather than just hand naming each folder
 def main():
     directoryName = 'Lab'
-    for i in range(1,13):
+    for number in range(1,13):
         
-        directoryName = directoryName + ' ' + str(i)
+        directoryName = directoryName + ' ' + str(number)
         try: 
-            os.mkdir(directoryName)
+            os.mkdir(directoryName) #Main command for making a directory
             print("Directory ", directoryName, " Created")
-        except:
+        except: 
+            #Catching potential errors of folders already existing
             print("Directory ", directoryName, " already exists")
         directoryName = 'Lab'
 
